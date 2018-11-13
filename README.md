d028302d304f96281be7aa55602b22580ee8685f
I403755 <j.boogaard@student.fontys.nl>
2018-11-13 11:23:09 +0100

    added new random dog function

--- README.md
+++ README.md
@@ -4,9 +4,10 @@ Simple Discord bot made using [Discord.js](https://discord.js.org/#/) and the [S
 
 ## List of commands
 
-> 1. `!gib shib/shibe/shiba/dog/doge/doggo` returns a shiba picture.
-> 2. `!gib cade/cat/cate/catto/kat/caddo` returns a cat picture.
-> 3. `!gib birb/bird/birdo/flapflap` returns a bird picture.
+> 1. `!gib shib/shibe/shiba` returns a shiba picture.
+> 2. `!gib dog/doge/doggo` returns a dog picture.
+> 3. `!gib cade/cat/cate/catto/kat/caddo` returns a cat picture.
+> 4. `!gib birb/bird/birdo/flapflap` returns a bird picture.
 
 ## How to add CaDoBiBot to your server
 
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const prefix = `!gib`;
 const shibURL = `http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true`;
 const cadeURL = `http://shibe.online/api/cats?count=1&urls=true&httpsUrls=true`;
 const birbURL = `http://shibe.online/api/birds?count=[1]&urls=true&httpsUrls=true`;
+const dogURL = `https://dog.ceo/api/breeds/image/random`;
 
 client.on(`ready`, () => {
     client.user.setActivity(`with my own feet`, {type: `PLAYING`});
@@ -17,7 +18,7 @@ client.on(`message`, msg => {
         const args = msg.content.slice(prefix.length).trim().split(/ +/g);
         command = args.shift().toLowerCase();
         switch(command){
-            case 'shib': case 'dog': case 'doggo': case 'doge': case 'shibe': case 'shiba':
+            case 'shib': case 'shibe': case 'shiba':
                 return new Promise((fulfill, reject) => {
                     getPic(shibURL).then((res) => {
                         if(res != ``){
@@ -49,14 +50,34 @@ client.on(`message`, msg => {
                         };
                     });
                 });
+
+            case 'dog': case 'doggo': case 'doge':
+                return new Promise((fulfill, reject) => {
+                    getPicCEO(dogURL).then((res) => {
+                        if(res != ``){
+                            fulfill(msg.channel.send({file: res}));
+                        }
+                    })
+                });
+
             default:
                 msg.channel.send(`not a valid command, fucko.`);
         }
-
+        // get image from shibe.online API
         function getPic(url){
             return new Promise((fulfill, reject) => {
                 fetch(url).then(res => res.json()).then((out) => {
                     fulfill(out[0]);
+                }).catch(err => { reject(err) });
+            });
+        };
+
+        // get image from dog.ceo API
+        function getPicCEO(url){
+            return new Promise((fulfill, reject) => {
+                fetch(url).then(res => res.json()).then((out) =>{
+                    console.log(out);
+                    fulfill(out.message);
                 }).catch(err => { reject(err) });
             });
         };
